ac6944319e13843a11d97fa2e4c3cfdf1150627e
ccavusoglu0 <cantug.cavusoglu@gmail.com>
2021-12-24 17:19:40 +0200

    Initial commit

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class TasksController extends Controller
+{
+    //
+    function save_file(Request $request)
+    {
+        $decoded_json = json_decode($request->getContent(), true);
+
+        //Create collection
+        $tasks_collection = collect($decoded_json['tasks']);
+
+        // Test //
+        // return $tasks_collection->firstWhere('name', 'rm');
+        // return $tasks_collection->all()
+
+        $sorted_tasks = $tasks_collection->sortDesc();
+
+        $commands = $sorted_tasks->pluck('command');
+
+        //Dosyaya satır satır yazdır
+        foreach ($commands as $command) {
+            Storage::disk('public')->append($decoded_json['filename'], $command);
+        }
+    }
+}
